7a60d7ce2b6c08bc2a22eabd43dd00217da069cb
Ramil <Warkinstar@mail.ru>
2023-05-25 23:17:35 +0600

    Adding user follow/unfollow actions with JS

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login, authenticate, get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from .models import Contact
 
 
 def user_login(request):
@@ -90,3 +92,24 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = get_user_model().objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user,
+                )
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except get_user_model().DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
