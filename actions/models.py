b991c8f0b06a6a92b2769e6e87b3052419249e3b
Ramil <Warkinstar@mail.ru>
2023-05-29 12:42:36 +0600

    Adding generic relations to your models

--- actions/models.py
+++ actions/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Action(models.Model):
@@ -7,10 +9,21 @@ class Action(models.Model):
         settings.AUTH_USER_MODEL, related_name="actions", on_delete=models.CASCADE
     )
     verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name="target_obj",
+        on_delete=models.CASCADE,
+    )
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_ct", "target_id")
+
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         indexes = [
             models.Index(fields=["-created"]),
+            models.Index(fields=["target_ct", "target_id"]),
         ]
         ordering = ["-created"]
