a88423086c3278ba28154198f432959b99b5cc42
Ramil <Warkinstar@mail.ru>
2023-05-29 16:39:49 +0600

    Using prefetch_related()

--- account/views.py
+++ account/views.py
@@ -43,7 +43,9 @@ def dashboard(request):
         actions = actions.filter(user_id__in=following_ids)
 
     # Actions->user->profile select_related query
-    actions = actions.select_related("user", "user__profile")[:10]
+    actions = actions.select_related("user", "user__profile").prefetch_related(
+        "target"
+    )[:10]
 
     return render(
         request, "account/dashboard.html", {"section": "dashboard", "actions": actions}
