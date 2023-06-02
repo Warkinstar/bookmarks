ec9ed895a7b409308099d3c218ecd56248a9b67e
Ramil <Warkinstar@mail.ru>
2023-06-02 19:56:53 +0600

    Django Debug Toolbar

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     "django_extensions",
     "easy_thumbnails",
     "django_cleanup",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -189,3 +191,7 @@ ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
 }
 
+# debug_toolbar
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
