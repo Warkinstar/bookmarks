534d723ad0db2eac56bdd1a15dc6cebba46529bc
Ramil <Warkinstar@mail.ru>
2023-06-03 02:06:30 +0600

    Storing image views in Redis

--- images/views.py
+++ images/views.py
@@ -8,12 +8,22 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse, HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+from django.conf import settings
+import redis
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.pk}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
@@ -78,6 +88,11 @@ def image_list(request):
         # If page out of range return last page of results
         images = paginator.page(paginator.num_pages)
     if images_only:
-        return render(request, "images/image/list_images.html", {"section": "images", "images": images})
-    return render(request, "images/image/list.html", {"section": "images", "images": images})
-
+        return render(
+            request,
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
+        )
+    return render(
+        request, "images/image/list.html", {"section": "images", "images": images}
+    )
